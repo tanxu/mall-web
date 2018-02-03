6c9ff65cb2bbcd23cd477560264625286462def7
Tank.tan <309313429@qq.com>
2018-02-03 14:44:40 +0800

    提交初始化项目

---
+++ src/components/NavBread.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="nav-breadcrumb-wrap">
+    <div class="container">
+      <nav class="nav-breadcrumb">
+        <a href="/">Home</a>
+        <slot name="break"></slot>
+      </nav>
+    </div>
+  </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        created() {
+        },
+        mounted() {
+        },
+        components: {},
+        methods: {}
+    }
+</script>
+<style scoped=""></style>
